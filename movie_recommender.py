6562e84df82288737260b3c0380175d5ac013a28
Bhumicodeshub <bhumisri0510@gmail.com>
2026-01-07 02:41:55 +0530

    add initial movie analysis logic
    Added functionality to load and display movie dataset.

--- movie_recommender.py
+++ movie_recommender.py
@@ -1,1 +1,15 @@
+import pandas as pd
 
+# Load the dataset
+movies = pd.read_csv("movies.csv")
+
+# Display basic information
+print("Total number of movies:", len(movies))
+print("\nFirst 5 movies:\n")
+print(movies.head())
+
+# Top rated movies
+top_movies = movies.sort_values(by="Rating", ascending=False).head(10)
+
+print("\nTop 10 Rated Movies:\n")
+print(top_movies[["Title", "Genre", "Rating"]])
